8df7a2070b763dca0989d8e970d64f61e22d8b27
deepu <deepakcsgec@gmail.com>
2016-06-19 20:03:39 +0530

    modified

--- virtualMachine.c
+++ virtualMachine.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
 #define PUSH(vm, v) vm->stack[++vm->sp] = v
 #define POP(vm)     vm->stack[vm->sp--]
 #define NCODE(vm)   vm->code[vm->pc++]
 
 #define STACK_SIZE 100
-int k=0,j=0,l=0;
 
 
 	 char co_varnames[] = {'a','b'};
          char co_names[] = {};
-         int co_consts[] = {0,1,2};
+         int co_consts[] = {0,10,20};
 
 
 enum {
@@ -25,16 +25,17 @@ enum {
 
 
 typedef struct {
-	int* loacls; // local scoped data
-	int* code;   // array od byte codes to be executed
-	int* stack;  // virtual stack
-	int pc;	     // program counter (aka IP - instruction pointer)
-	int sp;      // stack pointer
+	int* loacls; 
+	int* code;   
+	int* stack;  
+	int pc;	     
+	int sp;      
+	int fp;	     
 	}VM;
 
-VM* newVM(int* code,	// pointer to table containing a bytecode to be executed
- int pc,		// address of instruction to be invoked as first one - entrypoint/main func
- int datasize)		// total locals size required to perform a program operation
+VM* newVM(int* code,	
+ int pc,		
+ int datasize)		
 {
 
 	VM* vm = (VM*)malloc(sizeof(VM));
@@ -58,29 +59,34 @@ void delVM(VM* vm)
 
 void run(VM* vm)
 {
+
+	
 	do {
+		if(NCODE(vm) >= 0) 
+		{
 		int opcode = NCODE(vm);
-		int v, a=0,b=0;
+		int v=0, a=0,b=0;
 //		printf(" %d",opcode);
 		switch(opcode)
 		{
 		case LOAD_CONST:
-			k++;
-			v=co_consts[k];
-//			printf(" %d %d  \n",v,k);
-//			printf(" %d ",co_consts[k]);
-			PUSH(vm, co_consts[k]);
+			v=NCODE(vm);
+//			printf(" %d \n",v);
+			PUSH(vm, co_consts[v]);
+//		        printf(" %d ",co_consts[v]);
 			break;
+
 		case STORE_FAST:
-			v=POP(vm);
-			co_varnames[j]=v;
-			j++;
+			v=NCODE(vm);
+//			printf("%d \n",v);
+			co_varnames[v]=POP(vm);
 			break;
+
 		case LOAD_FAST:
-//			printf("%d ",co_varnames[l]);
-			PUSH(vm, co_varnames[l]);
-			l++;
+			v=NCODE(vm);
+			PUSH(vm, co_varnames[v]);
 			break;
+
 		case  BINARY_ADD:
 			a = POP(vm);
 //			printf("%d \n ",a);
@@ -94,49 +100,53 @@ void run(VM* vm)
 			printf(" %d \n",v);
 			return;
 			break;
+		default:
+			break;
 		}
+	}
 }while(1);
 
 }
 
 
 /* def sum1():
-        a=1
+	a=1
         b=2
         return a+b */
 
-/* dis.dis(sum1)
-   2	      0   LOAD_CONST               1
-              3   STORE_FAST               0 
 
-   3          6   LOAD_CONST               2 
-              9   STORE_FAST               1 
+/*
+	2     0 LOAD_CONST               1 (1)
+              3 STORE_FAST               0 (a)
 
-   4         12   LOAD_FAST                0 
-             15   LOAD_FAST                1 
-             18   BINARY_ADD                 
-             19   RETURN_VALUE               
+  3           6 LOAD_CONST               2 (2)
+              9 STORE_FAST               1 (b)
 
+  4          12 LOAD_FAST                0 (a)
+             15 LOAD_FAST                1 (b)
+             18 BINARY_ADD          
+             19 RETURN_VALUE   
 */
+
 
 int main()
 {
-	int program[] = {
-              LOAD_CONST,    
-               STORE_FAST,    
-
-               LOAD_CONST,   
-              STORE_FAST,   
-
-              LOAD_FAST,    
-              LOAD_FAST,    
-              BINARY_ADD,                 
-              RETURN_VALUE               
+	int program[]={
+  	0,LOAD_CONST,1,    
+	3,STORE_FAST,0,    
+        6,LOAD_CONST,2,   
+        9,STORE_FAST,1,    
+        12,LOAD_FAST,1,     
+        15,LOAD_FAST,0,     
+        18,BINARY_ADD,               
+        19,RETURN_VALUE
 };
 
 	VM* vm = newVM(program,0,10);
 	run(vm);
 }
+
+
 
 
 
