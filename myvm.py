8919b84d994ce4c55a827c215c26402ebbc347a1
deepu <deepakcsgec@gmail.com>
2016-06-27 16:08:15 +0530

    modified

--- myvm.py
+++ myvm.py
@@ -1,5 +1,6 @@
 from sys import argv
 
+
 LOAD_CONSTANT = 0x64
 LOAD_NAME = 0x65
 STORE_NAME = 0x5a
@@ -8,15 +9,23 @@ PRINT_NEWLINE = 0x48
 COMPARE_OP = 0x6b
 BINARY_ADD = 0x17
 BINARY_MULTIPLY = 0x14
+BINARY_DIVIDE = 0x15
 BINARY_SUBTRACT = 0x18
+BINARY_MODULO = 0x16
 POP_JUMP_IF_FALSE = 0x72
+POP_JUMP_IF_TRUE = 0x73
+JUMP_FORWARD = 0x6e
+JUMP_ABSOLUTE = 0x71
+SETUP_LOOP = 0x78
 POP_BLOCK = 0x57
 MAKE_FUNCTION = 0x84
 RETURN_VALUE = 0x53
+UNARY_NOT = 0xc
 CALL_FUNCTION = 0x83
 LOAD_FAST = 0x7c
 STORE_FAST = 0x7d
 LOAD_GLOBAL = 0x74
+POP_TOP = 0x1
 
 TYPE_TUPLE = 0x28
 TYPE_INTEGER = 0x69
@@ -51,41 +60,67 @@ class Stack(object):
     def print_stack(self):
         print self.stack
 
+
 stk = Stack()
 
-def binaryadd(current):
+
+def pop_top(current):
+    stk.pop()
+    return current+1
+
+
+def unary_not(current):
+    stk.push(not stk.pop())
+    return current + 1
+
+
+def binary_add(current):
     tos = stk.pop()
     tos1 = stk.pop()
     stk.push(tos1 + tos)
     return current + 1
 
 
-def binarymul(current):
+def binary_mul(current):
     tos = stk.pop()
     tos1 = stk.pop()
     stk.push(tos1 * tos)
     return current + 1
 
 
-def binarysub(current):
+def binary_sub(current):
     tos = stk.pop()
     tos1 = stk.pop()
     stk.push(tos1 - tos)
     return current + 1
 
 
-def printitem(current):
+def binary_div(current):
+    tos = stk.pop()
+    tos1 = stk.pop()
+    stk.push(tos1 / tos)
+    return current + 1
+
+
+def binary_modulo(current):
+    tos = stk.pop()
+    tos1 = stk.pop()
+    stk.push(tos1 % tos)
+    return current + 1
+
+
+def print_item(current):
     print stk.get_top_n(0),
     return current + 1
 
 
-def printnewline(current):
+def print_newline(current):
     print
     return current + 1
 
 
-def loadname(objcode, current):
-    oparg = objcode.getoparg(current)
+def load_name(objcode, current):
+    oparg = objcode.get_oparg(current)
     name = objcode.names[oparg]
     if type(name) == int:
         stk.push(name)
@@ -94,56 +129,75 @@ def loadname(objcode, current):
     return current + 3
 
 
-def loadconst(objcode, current):
-    oparg = objcode.getoparg(current)
+def load_const(objcode, current):
+    oparg = objcode.get_oparg(current)
     stk.push(objcode.consts[oparg])
     return current + 3
 
 
-def loadglobal(objcode, current):
-    oparg = objcode.getoparg(current)
+def load_global(objcode, current):
+    oparg = objcode.get_oparg(current)
     stk.push(objcode.names[oparg][0])
     return current + 3
 
 
-def loadfast(objcode, current):
-    oparg = objcode.getoparg(current)
+def load_fast(objcode, current):
+    oparg = objcode.get_oparg(current)
     stk.push(objcode.varnames[oparg])
     return current + 3
 
 
-def storename(objcode, current):
-    oparg = objcode.getoparg(current)
+def store_name(objcode, current):
+    oparg = objcode.get_oparg(current)
     objcode.names[oparg] = stk.pop()
     return current + 3
 
 
-def storefast(objcode, current):
-    oparg = objcode.getoparg(current)
+def store_fast(objcode, current):
+    oparg = objcode.get_oparg(current)
     objcode.varnames[oparg] = stk.pop()
     return current + 3
 
 
-def popblock(current):
+def setup_loop(objcode, current):
+    return current + 3
+
+
+def pop_block(current):
     return current + 1
 
 
-def popjumpiffalse(objcode, current):
+def pop_jump_if_false(objcode, current):
     if not stk.pop():
-        return objcode.getoparg(current)
+        return objcode.get_oparg(current)
     else:
         return current + 3
 
 
-def makefunction(objcode, current):
+def pop_jump_if_true(objcode, current):
+    if stk.pop():
+        return objcode.get_oparg(current)
+    else:
+        return current + 3
+
+
+def jump_forward(objcode, current):
+    return current + 3 + objcode.get_oparg(current)
+
+
+def jump_absolute(objcode, current):
+    return objcode.get_oparg(current)
+
+
+def make_function(objcode, current):
     return current + 9
 
 
-def lessthan(op1, op2):
+def less_than(op1, op2):
     return op1 < op2
 
 
-def lessequal(op1, op2):
+def less_equal(op1, op2):
     return op1 <= op2
 
 
@@ -151,43 +205,64 @@ def equal(op1, op2):
     return op1 == op2
 
 
+def not_equal(op1, op2):
+    return op1 != op2
+
+
+def greater_than(op1, op2):
+    return op1 > op2
+
+
+def grt_equal(op1, op2):
+    return op1 >= op2
+
 
 comparisons = {
-    0: lessthan,
-    1: lessequal,
-    2: equal
+    0: less_than,
+    1: less_equal,
+    2: equal,
+    3: not_equal,
+    4: greater_than,
+    5: grt_equal
 }
 
 
-def compareop(objcode, current):
+def compare_op(objcode, current):
     top = stk.pop()
     top1 = stk.pop()
-    oparg = objcode.getoparg(current)
+    oparg = objcode.get_oparg(current)
     stk.push(comparisons[oparg](top1, top))
     return current + 3
 
-
-
 operations = {
-    BINARY_ADD: binaryadd,
-    BINARY_SUBTRACT: binarysub,
-    BINARY_MULTIPLY: binarymul,
-    LOAD_NAME: loadname,
-    LOAD_FAST: loadfast,
-    LOAD_CONSTANT: loadconst,
-    LOAD_GLOBAL: loadglobal,
-    STORE_NAME: storename,
-    STORE_FAST: storefast,
-    PRINT_ITEM: printitem,
-    PRINT_NEWLINE: printnewline,
-    POP_JUMP_IF_FALSE: popjumpiffalse,
-    COMPARE_OP: compareop,
-    MAKE_FUNCTION: makefunction
-}
+    BINARY_ADD: binary_add,
+    BINARY_SUBTRACT: binary_sub,
+    BINARY_MULTIPLY: binary_mul,
+    BINARY_DIVIDE: binary_div,
+    BINARY_MODULO: binary_modulo,
+    LOAD_NAME: load_name,
+    LOAD_FAST: load_fast,
+    LOAD_CONSTANT: load_const,
+    LOAD_GLOBAL: load_global,
+    STORE_NAME: store_name,
+    STORE_FAST: store_fast,
+    PRINT_ITEM: print_item,
+    PRINT_NEWLINE: print_newline,
+    POP_JUMP_IF_FALSE: pop_jump_if_false,
+    POP_JUMP_IF_TRUE: pop_jump_if_true,
+    JUMP_FORWARD: jump_forward,
+    JUMP_ABSOLUTE: jump_absolute,
+    COMPARE_OP: compare_op,
+    UNARY_NOT: unary_not,
+    POP_TOP: pop_top,
+    MAKE_FUNCTION: make_function,
+    SETUP_LOOP: setup_loop,
+    POP_BLOCK: pop_block
+} 
 
 
 names = {}
-name_count = 0
+name_cnt = 0
 
 
 class Code(object):
@@ -198,25 +273,25 @@ class Code(object):
         self.code = self.fun_code()
         self.consts = self.fun_consts()
         self.names = self.fun_names()
-        self.varnames = self.fun_varnames()
-        self.name = self.fun_name()
+        self.varnames = self.fun_names()
+        self.name = self.acq_name()
 
-    def getname(self):
+    def get_name(self):
         return self.name
 
-    def getcurrent(self):
+    def get_current(self):
         return self.current
 
-    def getpyclist(self):
+    def get_pyclist(self):
         return self.pyclist
 
-    def getopcode(self, current):
+    def get_opcode(self, current):
         return self.code[current]
 
-    def getoparg(self, current):
+    def get_oparg(self, current):
         return decimal(self.code, current)
 
-    def Isend(self, current):
+    def is_end(self, current):
         if current >= len(self.code):
             return True
         else:
@@ -224,12 +299,12 @@ class Code(object):
 
     def fun_code(self):
         pyclist = self.pyclist
-        current = startofcode(pyclist, self.current)
+        current = start_of_code(pyclist, self.current)
         end = current + decimal(pyclist, current-5, 4)
         code = []
         while current < end:
             if not is_func_def(current, pyclist):
-                if havearg(pyclist[current]):
+                if have_arg(pyclist[current]):
                     code.extend(pyclist[current:current+3])
                     current += 3
                 else:
@@ -242,6 +317,7 @@ class Code(object):
 
         self.current = current
         return code
+
 
     def fun_consts(self):
         current = self.current
@@ -258,34 +334,35 @@ class Code(object):
                 current += 1
             elif pyclist[current] == TYPE_CODE:
                 objcode = Code(pyclist, current)
-                f_index = objcode.getname()
+                f_idx = objcode.get_name()
                 consts.append(objcode)
-                names[f_index][0] = objcode
+                names[f_idx][0] = objcode
                 current = end_of_code(pyclist, current)
 
         self.current = current
         return consts
 
     def fun_names(self):
-        global name_count
+        global name_cnt
         current = self.current
         pyclist = self.pyclist
         n_names = decimal(pyclist, current)
-        func_index = 0
+        func_idx = 0
         current += 5
         co_names = {}
-        index = 0
+        idx = 0
         for dummy in range(n_names):
+            # first occurrentrence of a name
             if (pyclist[current] == TYPE_INTERN):
-                names[name_count] = [0]
-                co_names[index] = names[name_count]
-                name_count += 1
-                index += 1
+                names[name_cnt] = [0]
+                co_names[idx] = names[name_cnt]
+                name_cnt += 1
+                idx += 1
                 current = skip_element(pyclist, current)
             elif (pyclist[current] == TYPE_SREF):
-                func_index = decimal(pyclist, current)
-                co_names[index] = names[func_index]
-                index += 1
+                func_idx = decimal(pyclist, current)
+                co_names[idx] = names[func_idx]
+                idx += 1
                 current += 5
             else:
                 current += 1
@@ -293,8 +370,8 @@ class Code(object):
         self.current = current
         return co_names
 
-    def fun_varnames(self):
-        global name_count
+    def fun_names(self):
+        global name_cnt
         current = self.current
         pyclist = self.pyclist
         varnames = []
@@ -303,8 +380,8 @@ class Code(object):
         for dummy in range(n_varnames):
             varnames.append(0)
             if pyclist[current] == TYPE_INTERN:
-                names[name_count] = [0]
-                name_count += 1
+                names[name_cnt] = [0]
+                name_cnt += 1
                 current = skip_element(pyclist,  current)
             elif pyclist[current] == TYPE_SREF:
                 current += 5
@@ -314,8 +391,8 @@ class Code(object):
         self.current = current
         return varnames
 
-    def fun_name(self):
-        global name_count
+    def acq_name(self):
+        global name_cnt
         current = self.current
         pyclist = self.pyclist
         n_field = 0
@@ -330,32 +407,33 @@ class Code(object):
         current = skip_element(pyclist, current)
         self.current = current
         if pyclist[current] == TYPE_INTERN:
-            names[name_count] = [0]
-            name_count += 1
-            return name_count - 1
+            names[name_cnt] = [0]
+            name_cnt += 1
+            return name_cnt - 1
 
         else:
             return decimal(pyclist, current, 4)
 
     def view(self):
-        print showpyc(self.code)
+        print "****************"
+        print show_pyc(self.code)
         print len(self.consts), 'constants'
-        for index in range(len(self.consts)):
-            if type(self.consts[index]) == int:
-                print self.consts[index]
+        for idx in range(len(self.consts)):
+            if type(self.consts[idx]) == int:
+                print self.consts[idx]
             else:
-                self.consts[index].view()
+                self.consts[idx].view()
         print self.names
         print self.varnames
         print self.name
         print 'global names'
         print names
+        print '--------------------------------'
+
+INT_LIMIT = 2 ** 20
 
 
-limit = 2 ** 20
-
-
-def readbytes(filename):
+def read_bytes(filename):
     with open(filename, "rb") as file_h:
         while True:
             chunk = file_h.read()
@@ -366,27 +444,27 @@ def readbytes(filename):
                 break
 
 
-def getpyclist(filename):
-    lst = list(readbytes(filename))
+def get_pyc_list(filename):
+    lst = list(read_bytes(filename))
     return lst
 
 
-def showpyc(lst):
+def show_pyc(lst):
     return [hex(num) for num in lst]
 
 
 def decimal(pyc_list, current, num_byte=2):
     value = 0
     factor = 0
-    for index in range(num_byte):
-        value |= pyc_list[current+index+1] << factor
+    for idx in range(num_byte):
+        value |= pyc_list[current+idx+1] << factor
         factor += 8
-    if value >= limit:
-        value = value - 2 * limit
+    if value >= INT_LIMIT:
+        value = value - 2 * INT_LIMIT
     return value
 
 
-def startofcode(pyc_list, current=0):
+def start_of_code(pyc_list, current=0):
     while (pyc_list[current] != TYPE_CODE and
             pyc_list[current+17] != TYPE_STRING):
         current += 1
@@ -401,6 +479,8 @@ def skip_element(pycbuf, current):
 
 
 def end_of_code(pyc_list, current=0):
+ 
+
     current += 17  
 
     current = skip_element(pyc_list, current)  
@@ -426,14 +506,16 @@ def end_of_code(pyc_list, current=0):
 
     current += 5
     current = skip_element(pyc_list, current)
+
     current = skip_element(pyc_list, current)
 
     current += 4
+
     current = skip_element(pyc_list, current)
     return current
 
 
-def havearg(opcode):
+def have_arg(opcode):
     if opcode < HAVE_ARG:
         return False
     else:
@@ -448,11 +530,14 @@ def is_func_def(current, pycbuf):
 
 
 def get_op_arg(pycbuf, current):
+
     oparg = decimal(pycbuf, current)
     return oparg
 
+
 def call_function(objcode, current):
-    argc = objcode.getoparg(current)
+
+    argc = objcode.get_oparg(current)
     func = stk.get_top_n(argc)
 
     bkup_locals = func.varnames[:]
@@ -463,22 +548,27 @@ def call_function(objcode, current):
 
     stk.pop()
     execute(func)
+
     func.varnames = bkup_locals[:]
+
     return current + 3
 
 
 def execute(objcode):
+   
     current = 0
-    while not objcode.Isend(current):
-        opcode = objcode.getopcode(current)
+    while not objcode.is_end(current):
+        opcode = objcode.get_opcode(current)
+
         if opcode == CALL_FUNCTION:
             current = call_function(objcode, current)
             continue
 
         if opcode == RETURN_VALUE:
             return
+
         try:
-            if havearg(opcode):
+            if have_arg(opcode):
                 current = operations[opcode](objcode, current)
             else:
                 current = operations[opcode](current)
@@ -489,7 +579,9 @@ def execute(objcode):
 
 
 def execute_pyc(pyc_file):
-    pyc_lst = getpyclist(pyc_file)
+    
+    pyc_lst = get_pyc_list(pyc_file)
+
     objcode = Code(pyc_lst)
 
     print
@@ -498,6 +590,7 @@ def execute_pyc(pyc_file):
 
 
 def main():
+
         pyc_file = argv[1]
         execute_pyc(pyc_file)
 
